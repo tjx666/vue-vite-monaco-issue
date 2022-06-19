ef0ff0bf532716dd519682589c3bc156ed113215
yutengjing <ytj2713151713@gmail.com>
2022-06-19 17:21:09 +0800

    vue vite editor.setValue example

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import MonacoEditor from './components/monacoEditor/MonacoEditor.vue';
+
+const source = ref(
+    JSON.stringify(
+        {
+            name: 'ly',
+            age: 18,
+            alone: true,
+        },
+        null,
+        4,
+    ),
+);
+
+setTimeout(() => {
+    source.value = JSON.stringify({
+        name: 'xxx',
+    });
+}, 3000);
+
+</script>
+
+<template>
+    <MonacoEditor language="json" :source="source" />
+</template>
+
+<style lang="scss">
+@import './normalize.css';
+
+html,
+body,
+#app {
+    width: 100vw;
+    height: 100vh;
+}
+
+#app {
+    .editor {
+        width: 100%;
+        height: 100%;
+    }
+}
+</style>
---
+++ src/components/monacoEditor/MonacoEditor.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref, onMounted, watch, toRefs } from 'vue';
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import './userWorker';
+
+type Editor = ReturnType<typeof monaco.editor.create>;
+type SupportedLanguage = 'typescript' | 'javascript' | 'json';
+
+interface EditorProps {
+    source: string;
+    language: SupportedLanguage;
+}
+const props = defineProps<EditorProps>();
+const { source, language } = toRefs(props);
+
+const editorContainer = ref<HTMLDivElement | null>(null);
+const editor = ref<Editor | null>(null);
+
+onMounted(() => {
+    editor.value = monaco.editor.create(editorContainer.value!, {
+        value: source.value,
+        language: language.value,
+        theme: 'vs-dark',
+        minimap: {
+            enabled: false,
+        },
+    });
+});
+
+watch(source, (newSource, oldSource) => {
+    if (newSource !== oldSource) {
+        console.log('change value');
+        editor.value?.setValue(newSource);
+    }
+});
+
+watch(language, (newLanguage: SupportedLanguage) => {
+    monaco.editor.setModelLanguage(editor.value!.getModel()!, newLanguage);
+});
+
+defineExpose({
+    editorContainer,
+});
+</script>
+
+<template>
+    <div class="editor" ref="editorContainer"></div>
+</template>
+
+<style lang="scss"></style>
